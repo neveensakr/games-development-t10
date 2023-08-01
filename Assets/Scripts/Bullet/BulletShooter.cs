332a1efc118a76573965ba8dd0900ab9646d0af9
Kapthas59 <kapthas59@gmail.com>
2023-08-01 17:45:05 +0800

    Updated the Player Character Graphics In Game

---
+++ Assets/Scripts/Bullet/BulletShooter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShooter : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform from;
+
+    public void Fire()
+    {
+        GameObject projectile = Instantiate(bullet, from.position, from.rotation);
+    }
+}
