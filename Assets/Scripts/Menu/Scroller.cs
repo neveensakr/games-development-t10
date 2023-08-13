ac6251a850ea28bc2d8cb040db3a27d07e53d6a0
Faizan Javair <faizanjavair@gmail.com>
2023-08-13 17:45:55 +0500

    Suggested changes implemented in code
    .

--- Assets/Scripts/Menu/Scroller.cs
+++ Assets/Scripts/Menu/Scroller.cs
@@ -5,13 +5,12 @@ using UnityEngine.UI;
 
 public class Scroller : MonoBehaviour
 {
-    [SerializeField] private RawImage _img;
-    [SerializeField] private float _x, _y;
+    [SerializeField] private RawImage image;
+    [SerializeField] private float x_axis, y_axis;
 
     // Update is called once per frame
-    void Update()
-    {
-        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size);
-        
+    void Update(){
+    // Code for asteroid scroller
+        image.uvRect = new Rect(image.uvRect.position + new Vector2(x_axis, y_axis) * Time.deltaTime, image.uvRect.size);
     }
 }
