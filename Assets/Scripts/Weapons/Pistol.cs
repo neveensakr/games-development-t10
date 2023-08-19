2cfd960a01c1ba1980f7a8233bb090562984e5d7
neveensakr <64664334+neveensakr@users.noreply.github.com>
2023-08-19 19:38:45 +0200

    Add Ability to Switch Skins
    There are three skins the player can have; Fire, Frost, and Lightning. The player can switch between these skins using the 1, 2, 3 keys. This happens via the Elements, Skins, Arms, and Bullets array where in each array in the same order, there is the Sprite/Prefab that represents that element. For example. the first element in all arrays is Fire.
    
    The player can also switch between two weapons. I have added an Abstract weapon class since the only difference between weapons is the way they Fire.
    
    The bullets act differently based on the element. This is yet to be implemented.

---
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon
+{
+    public override void Fire(GameObject bulletPrefab)
+    {
+        Debug.Log("Pistol Firing!");
+        Bullet bullet = Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation).GetComponent<Bullet>();
+        bullet.owner = Characters.Player;
+        bullet.bulletDamage = Damage;
+    }
+}
---
+++ Assets/Scripts/Weapons/ShotGun.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGun : Weapon
+{
+    public override void Fire(GameObject bulletPrefab)
+    {
+        Debug.Log("Shotgun Firing!");
+    }
+}
