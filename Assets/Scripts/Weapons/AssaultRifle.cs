4150bd511a2f1e05b08c9d9ab146a692c9aa84cb
Faizan Javair <faizanjavair@gmail.com>
2023-08-26 21:49:14 +0500

    Updated Assault Rifle Code

--- Assets/Scripts/Weapons/AssaultRifle.cs
+++ Assets/Scripts/Weapons/AssaultRifle.cs
@@ -10,19 +10,14 @@ public class AssaultRifle : Weapon
     [SerializeField] private float fireRate = 0.1f;
     private float nextFireTime = 0f;
 
-    private void Update()
-    {
-        if (IsActive && Time.time > nextFireTime && Input.GetButton("Fire1"))
-        {
-            Fire(bulletPrefab);
-            nextFireTime = Time.time + fireRate;
-        }
-    }
-
     public override void Fire(GameObject bulletPrefab)
     {
-        Bullet bullet = Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation).GetComponent<Bullet>();
-        bullet.owner = Characters.Player;
-        bullet.bulletDamage = Damage;
+        while (IsActive && Time.time > nextFireTime)
+        {
+            Bullet bullet = Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation).GetComponent<Bullet>();
+            bullet.owner = Characters.Player;
+            bullet.bulletDamage = Damage;
+            nextFireTime = Time.time + fireRate;
+        }
     }
 }
